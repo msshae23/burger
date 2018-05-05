f7f2483b128316f6c610e802769d7964fe6300d0
Shaela Sterling <msshae23_09@yahoo.com>
2018-05-05 04:54:17 -0400

    still buggy

---
+++ config/connection.js
@@ -0,0 +1,31 @@
+// import all required
+
+var mysql = require("mysql");
+var PORT = process.env.PORT || 3306;
+
+//connection variable
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+    port: PORT,
+    host: "localhost",
+    user: "root",
+    password: "SQLSterling0220",
+    database:"burgers_db"
+});
+};
+
+
+
+// Make connection.
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured) VALUES("The Best Lamb Burger", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES("Double Bacon Cheeseburger", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES("Turkey Burger", FALSE);
